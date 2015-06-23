5ffe81e3016c771a63ee6559791a2f7b33a8461d
Borja <borja@libcrack.so>
2015-06-24 00:00:55 +0200

    Added try/except statements for file open actions

--- gitrecon.py
+++ gitrecon.py
@@ -15,7 +15,7 @@ if sys.version[0] == '3':
 
 MYPATH = os.path.abspath(os.path.expanduser(__file__))
 if os.path.islink(MYPATH):MYPATH = os.readlink(MYPATH)
-MYLIBPATH = os.path.dirname(MYPATH) + "/lib/"
+MYLIBPATH = os.path.dirname(MYPATH) + '/lib/'
 sys.path.append(os.path.dirname(MYLIBPATH))
 
 import Colors
@@ -57,17 +57,17 @@ if halt:
 
 
 def logo():
-    print("%s       _ _                            " % (colors.red))
-    print("  __ _(_) |_ _ __ ___  ___ ___  _ __  ")
-    print(" / _` | | __| '__/ _ \/ __/ _ \| '_ \ ")
-    print("| (_| | | |_| | |  __/ (_| (_) | | | |")
-    print(" \__, |_|\__|_|  \___|\___\___/|_| |_|")
-    print(" |___/                                ")
-    print("%s" % colors.reset)
-    print("Authors:")
-    print("    Jaime Filson aka WiK <wick2o@gmail.com>")
-    print("    Borja Ruiz <borja@libcrack.so>")
-    print("License: BSD (3-Clause)\n")
+    print('%s       _ _                            ' % (colors.red))
+    print('  __ _(_) |_ _ __ ___  ___ ___  _ __  ')
+    print(' / _` | | __| \'__/ _ \/ __/ _ \| \'_ \ ')
+    print('| (_| | | |_| | |  __/ (_| (_) | | | |')
+    print(' \__, |_|\__|_|  \___|\___\___/|_| |_|')
+    print(' |___/                                ')
+    print('%s' % colors.reset)
+    print('Authors:')
+    print('    Jaime Filson aka WiK <wick2o@gmail.com>')
+    print('    Borja Ruiz <borja@libcrack.so>')
+    print('License: BSD (3-Clause)\n')
 
 
 def parse_args():
@@ -148,7 +148,7 @@ def main():
     if args.debug:
         logger.debug('Creating a database in memory')
 
-    db = sqlite3.connect(":memory:")
+    db = sqlite3.connect(':memory:')
     cur = db.cursor()
     cur.execute('CREATE TABLE files (id INTEGER PRIMARY KEY, name \
                 varchar(255) UNIQUE, count int DEFAULT 1)')
@@ -162,54 +162,61 @@ def main():
         try:
             for m_file in files:
                 try:
-                    cur.execute("INSERT INTO files ('name') VALUES (?)",
+                    cur.execute('INSERT INTO files (\'name\') VALUES (?)',
                             (os.path.join(root, m_file)))
                 except sqlite3.IntegrityError:
-                    cur.execute("UPDATE files SET count = count + 1 WHERE \
-                    name = ?", (os.path.join(root, m_file)))
+                    cur.execute('UPDATE files SET count = count + 1 WHERE \
+                    name = ?', (os.path.join(root, m_file)))
             for m_dir in dirs:
                 try:
-                    cur.execute("INSERT INTO dirs ('name') VALUES (?)",
+                    cur.execute('INSERT INTO dirs ('name') VALUES (?)',
                             (os.path.join(root, m_dir)))
                 except sqlite3.IntegrityError:
-                    cur.execute("UPDATE dirs SET count = count + 1 \
-                               WHERE name = ?" % (os.path.join(root, m_dir)))
+                    cur.execute('UPDATE dirs SET count = count + 1 \
+                               WHERE name = ?' % (os.path.join(root, m_dir)))
         except sqlite3.OperationalError as e:
-            logger.exception("Something awful happened!")
+            logger.exception('Something awful happened!')
             print(traceback.format_exc())
 
     if args.debug:
         logger.debug('Generating the files wordlist')
 
-    cur.execute("SELECT name FROM files ORDER BY count DESC")
-    res = cur.fetchall()
+    try:
+        cur.execute('SELECT name FROM files ORDER BY count DESC')
+        res = cur.fetchall()
+    except sqlite3.OperationalError as e:
+        logger.error('Error getting file list from database')
 
     filename = '%s/%s-files.txt' % (args.username,args.username)
     logger.info('Writing %s' % filename)
-    fp = open(filename, 'w')
-
-    for itm in res:
-        encoded_itm = itm[0].encode('utf8')
-        if isinstance(itm[0],basestring):
+    try:
+        fp = open(filename, 'w')
+        for itm in res:
             encoded_itm = itm[0].encode('utf8')
-        else:
-            encoded_itm = unicode(itm[0]).encode('utf8')
-        fp.write('%s\n' % (encoded_itm))
-    fp.close()
+            if isinstance(itm[0],basestring):
+                encoded_itm = itm[0].encode('utf8')
+            else:
+                encoded_itm = unicode(itm[0]).encode('utf8')
+            fp.write('%s\n' % (encoded_itm))
+        fp.close()
+    except (OSError, IOError):
+        logger.error('Cannot write to %s' % filename)
 
     if args.debug:
         logger.debug('Generating the dirs wordlist')
 
-    cur.execute("SELECT name FROM dirs ORDER BY count DESC")
+    cur.execute('SELECT name FROM dirs ORDER BY count DESC')
     res = cur.fetchall()
 
     filename = '%s/%s-dirs.txt' % (args.username,args.username)
-    fp = open(filename, 'w')
     logger.info('Writing %s' % filename)
-
-    for itm in res:
-        fp.write('%s\n' % (itm[0]))
-    fp.close()
+    try:
+        fp = open(filename, 'w')
+        for itm in res:
+            fp.write('%s\n' % (itm[0]))
+        fp.close()
+    except (OSError, IOError):
+        logger.error('Cannot write to %s' % filename)
 
     if cur:
         cur.close()
@@ -219,6 +226,13 @@ def main():
         del db
 
     logger.info('Cloned %s repos from http://www.github.com/%s' % (downloaded_repos,args.username))
+
+    try:
+        os.move(os.path.abspath('./gitrecon.log'), os.path.abspath('%s/gitrecon.log' % args.username))
+    except:
+        logger.error('Cannot move logfile to %s' % os.path.abspath(args.username))
+    else:
+        logger.info('Logfile saved %s' % os.path.abspath('%s/gitrecon.log' % args.username))
 
 if __name__ == '__main__':
     main()
